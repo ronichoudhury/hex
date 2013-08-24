35f5ade50c1dc3e63b5189f9cf52efb995e51af5
Roni Choudhury <roni.choudhury@kitware.com>
2013-08-24 18:07:20 -0400

    Delinted initial code.

--- hex.hs
+++ hex.hs
@@ -34,21 +34,20 @@ parse input
         (varname, value) = break (== '=') input
 
 perform :: Command -> Environment -> IO Environment
-perform cmd env = do
-    case cmd of
-        Assign var valtext ->
-            let val = readMaybe valtext in
-               case val of
-                   Just v -> return $ insert (strip var) v env
-                   Nothing -> do putStrLn $ "error: could not parse value '" ++ (strip valtext) ++ "'"
-                                 return env
-        Report vartext -> let var = strip vartext
-	                      val = Data.Map.lookup var env in
-                          do case val of
-                                 Just v -> putStrLn $ show v
-                                 Nothing -> putStrLn $ "error: no such variable '" ++ (strip var) ++ "'"
+perform cmd env = case cmd of
+    Assign var valtext ->
+        let val = readMaybe valtext in
+           case val of
+               Just v -> return $ insert (strip var) v env
+               Nothing -> do putStrLn $ "error: could not parse value '" ++ strip valtext ++ "'"
                              return env
-        Quit -> error "perform called with Quit"
+    Report vartext -> let var = strip vartext
+                          val = Data.Map.lookup var env in
+                      do case val of
+                             Just v -> print v
+                             Nothing -> putStrLn $ "error: no such variable '" ++ strip var ++ "'"
+                         return env
+    Quit -> error "perform called with Quit"
 
 repl :: Environment -> IO ()
 repl e = do
@@ -57,11 +56,10 @@ repl e = do
     input <- getLine
     if strip input == ""
         then repl e
-        else do command <- return $ parse input
-	        if isQuit command
-                    then return ()
-                    else do e' <- perform command e
-			    repl e'
+        else let command = parse input in
+             unless (isQuit command) $
+                 do e' <- perform command e
+                    repl e'
 
 main :: IO ()
 main = repl empty
